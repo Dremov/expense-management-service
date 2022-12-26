0adcfaffce15ffaeff24573e7aba8f38b8c24b00
Dremov <andrew.dremov@gmail.com>
2022-12-26 21:15:06 +0100

    Initial commit

---
+++ src/main/java/com/dremov/expensemanagementservice/ExpenseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dremov.expensemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dremov/expensemanagementservice/ExpenseManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dremov.expensemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
